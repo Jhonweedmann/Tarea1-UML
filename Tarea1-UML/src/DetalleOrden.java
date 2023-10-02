6c0280a1d58ec993380d9cacb31b21ed598b00c5
Jhonweedmann <benjaminjr2004@gmail.com>
2023-10-02 20:52:50 -0300

    fix: Ahora el precio se calcula correctamente dependiendo de la cantidad de articulos

--- Tarea1-UML/src/DetalleOrden.java
+++ Tarea1-UML/src/DetalleOrden.java
@@ -1,17 +1,20 @@
 public class DetalleOrden {
     private Articulo a = new Articulo();
-    private float IVA = (a.getPrecio()*19)/100;
+
+    //private float IVA = (a.getPrecio()*19)/100;
+    private int cantidad;
     public DetalleOrden(){
     }
+
     public float calcPrecio(){
 
-        return (a.getPrecio() + IVA);
+        return (a.getPrecio() + ((a.getPrecio()*19)/100));
     }
     public float calcPrecioSinIVA(){
         return a.getPrecio();
     }
     public float calcIVA(){
-        return IVA;
+        return (a.getPrecio()*19)/100;
     }
     public int calcPeso(){
         return 0;
