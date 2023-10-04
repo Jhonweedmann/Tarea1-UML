3093faed2b9b3ed3914938a458ba979b40b5b769
Jhonweedmann <benjaminjr2004@gmail.com>
2023-10-04 15:53:43 -0300

    feat & fix: Ahora se puede guardar informacion de más de un DetalleOrden y OrdenCompra calculará correctamente los parametros sin importar la cantidad de este.

--- Tarea1-UML/src/DetalleOrden.java
+++ Tarea1-UML/src/DetalleOrden.java
@@ -1,22 +1,29 @@
 public class DetalleOrden {
-    private Articulo a = new Articulo();
 
-    //private float IVA = (a.getPrecio()*19)/100;
+    private Articulo a;
+
     private int cantidad;
-    public DetalleOrden(){
+    public DetalleOrden(Articulo a, int cantidad) {
+        this.a = a;
+        this.cantidad = cantidad;
+        //OrdenCompra o;
+    }
+
+    public int getCantidad() {
+        return cantidad;
     }
 
     public float calcPrecio(){
 
-        return (a.getPrecio() + ((a.getPrecio()*19)/100));
+        return ((a.getPrecio() + ((a.getPrecio()*19)/100)))*cantidad;
     }
     public float calcPrecioSinIVA(){
-        return a.getPrecio();
+        return (a.getPrecio())*cantidad;
     }
     public float calcIVA(){
-        return (a.getPrecio()*19)/100;
+        return ((a.getPrecio()*19)/100)*cantidad;
     }
-    public int calcPeso(){
-        return 0;
+    public float calcPeso(){
+        return a.getPeso();
     }
 }
--- Tarea1-UML/src/OrdenCompra.java
+++ Tarea1-UML/src/OrdenCompra.java
@@ -1,23 +1,52 @@
+import java.util.ArrayList;
 import java.util.Date;
 
 public class OrdenCompra {
-    DetalleOrden DO;
+    private DetalleOrden DO;
+    private Articulo articulo;
+
     private Date fecha;
+
     private String estado;
-    public OrdenCompra(){
-        DO = new DetalleOrden();
+    private ArrayList<DetalleOrden> Lista;
+    /* El constructor acepta un Detalle de orden y lo agregará automaticamente al ArrayList de DetalleOrden. Si se quieren agregar más de uno se tiene que ocupar el metodo "addDetalleOrden"
+    */
+    public OrdenCompra(DetalleOrden DO) {
+        this.DO = DO;
+        Lista = new ArrayList<>();
+        Lista.add(DO);
     }
-    public int calcPrecioSinIVA(){
-    return 0;
+
+    public void addDetalleOrden(DetalleOrden DO2){
+        Lista.add(DO2);
+    }
+    public float calcPrecioSinIVA(){
+        int sum = 0;
+        for(int i=0;i<Lista.size(); i++){
+            sum += Lista.get(i).calcPrecioSinIVA();
+        }
+    return sum;
     }
     public float calcIVA(){
-        return DO.calcIVA();
+        int sum = 0;
+        for(int i=0;i<Lista.size();i++){
+            sum += Lista.get(i).calcIVA();
+        }
+        return sum;
     }
     public float calcPrecio(){
-        return DO.calcPrecio();
+        int sum = 0;
+        for(int i=0;i<Lista.size();i++){
+            sum += Lista.get(i).calcPrecio();
+        }
+        return sum;
     }
-    public int calcPeso(){
-        return DO.calcPeso();
+    public float calcPeso(){
+        int sum = 0;
+        for(int i=0;i<Lista.size();i++){
+            sum += Lista.get(i).calcPeso();
+        }
+        return sum;
     }
 
 }
