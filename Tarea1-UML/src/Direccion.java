647a1b3492e9d5f0034e989af32a063beda2bcd6
Jhonweedmann <benjaminjr2004@gmail.com>
2023-10-04 23:15:07 -0300

    fix: Ya no se necesita llamar a un setter para guardar direccion

--- Tarea1-UML/src/Direccion.java
+++ Tarea1-UML/src/Direccion.java
@@ -1,3 +1,12 @@
 public class Direccion {
     private String direccion;
+    private Cliente cliente;
+
+    public Direccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
 }
